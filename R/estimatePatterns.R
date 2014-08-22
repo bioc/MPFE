a6b3673bc8478b59d9fe276a084c33f10447205c
Marc Carlson <mcarlson@fhcrc.org>
2014-08-22 20:00:17 +0000

    Adds ClassifyR/ M3D/ miRNAtap/ MPFE/ RUVnormalize/ SemDist/ to the repos.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MPFE@93529 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/estimatePatterns.R
@@ -0,0 +1,58 @@
+estimatePatterns <- function(patternCounts,
+                             epsilon=0,
+                             eta=0,
+                             column=NULL,
+                             fast=TRUE,
+                             steps=20000)
+{
+    # Check the patterns
+    # First, strip any leading character different from 01
+    patterns <- gsub('^[^01]', '', as.character(patternCounts[, 1]))
+    # Make sure we only have zeros and ones:
+    matches <- grep('[01]+', patterns, invert=TRUE)
+    if (length(matches) > 0) {
+        stop('Only 0s and 1s allowed in pattern. First (up to ten) offending patterns:', head(patterns[matches], 10), '\n')
+    }
+    # Make sure that all patterns have the same length
+    nCpGsites <- nchar(patterns[1])
+    if (any(nchar(patterns) != nCpGsites)) {
+        stop('All patterns must be the same length\n')
+    }
+
+    patternCounts[, 1] <- patterns
+    # Check Epsilon
+    if (!is.numeric(epsilon) || epsilon < 0 || epsilon >= 1) {
+        stop('epsilon must be a numeric between 0 and 1\n')
+    }
+
+    # Check Eta
+    nEtas <- length(eta)
+    if (nEtas != 1 && nEtas != nCpGsites) {
+        stop('Length of eta is not equal to the number of CpG sites\n')
+    } else if (!is.numeric(eta) || any(eta < 0) || any(eta >= 1)) {
+        stop('eta must be a numeric between 0 and 1\n')
+    }
+
+    # Check the column indices
+    nColumns <- ncol(patternCounts) - 1
+    if (is.null(column)) {
+        columns <- 1:nColumns
+    } else {
+        columns <- column
+    }
+    if (any(columns < 1 || columns > nColumns)) {
+        stop('Column indices must be between 1 and ', nColumns, '\n')
+    }
+
+    compareData <- list()
+    for (i in 1:length(columns)) {
+        compareData[[i]] <- estimatePatternsOneColumn(patternCounts,
+                                                      epsilon,
+                                                      eta,
+                                                      column=columns[i],
+                                                      fast,
+                                                      steps)
+    }
+
+    return(compareData)
+}
